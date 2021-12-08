ae95087b34be7405e4b8fec309f3cb2909e662f5
abdullahhhkeskinn <abdullahhhkeskinn@gmail.com>
2021-12-08 22:24:52 +0300

    Gyroscope support added

--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -4,20 +4,36 @@ using UnityEngine;
 
 public class InputManager : MonoBehaviour
 {
-    float horizontal;
-    float vertical;
+   private bool gyroEnabled;
+   private Gyroscope gyro;
+   private GameObject cameraContainer;
+   private Quaternion rot;
 
-    // Start is called before the first frame update
-    void Start()
-    {
+    private void Start(){
+        Debug.Log("Start");
         
+        cameraContainer = new GameObject("Camera container");
+        cameraContainer.transform.position = transform.position;
+        transform.SetParent(cameraContainer.transform);
+
+        gyroEnabled = EnableGyro();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        horizontal = Input.GetAxis("Mouse X");
-        vertical = Input.GetAxis("Mouse Y");
+    private bool EnableGyro(){
+        if(SystemInfo.supportsGyroscope){
+            gyro = Input.gyro;
+            gyro.enabled = true;
+            cameraContainer.transform.rotation = Quaternion.Euler(90f,180f,0);
+            rot = new Quaternion(0,0,1,0);
 
+            return true;
+        }
+        return false;
+    }
+    
+    private void Update(){
+        if(gyroEnabled){
+            transform.localRotation = gyro.attitude * rot;
+        }
     }
 }

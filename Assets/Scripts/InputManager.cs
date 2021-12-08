3f05cedd886c763da74519033b2217f2b2561afa
Beytullah Çetin <44231507+BeytullahCetin@users.noreply.github.com>
2021-12-08 20:15:02 +0300

    Add gyro camera rotation

--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -4,20 +4,45 @@ using UnityEngine;
 
 public class InputManager : MonoBehaviour
 {
+
+    public static InputManager Instance { get; private set; }
+
+
     float horizontal;
     float vertical;
+    public Quaternion gyro;
 
-    // Start is called before the first frame update
-    void Start()
+    private void Awake()
     {
-        
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void Start()
+    {
+        if (SystemInfo.supportsGyroscope)
+            Input.gyro.enabled = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        horizontal = Input.GetAxis("Mouse X");
-        vertical = Input.GetAxis("Mouse Y");
+        /* horizontal = Input.GetAxis("Mouse X");
+        vertical = Input.GetAxis("Mouse Y"); */
+        if(SystemInfo.supportsGyroscope)
+            gyro = GyroToUnity(Input.gyro.attitude);
 
+    }
+
+    Quaternion GyroToUnity(Quaternion q)
+    {
+        return new Quaternion(q.x, q.y, -q.z, -q.w);
     }
 }

65d4a4ad22cf8bda68855c90fc225fb0401a39d1
Beytullah Çetin <44231507+BeytullahCetin@users.noreply.github.com>
2021-12-09 19:09:16 +0300

    Update folder structure

---
+++ Assets/Scripts/GyroManager.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GyroManager : MonoBehaviour
+{
+   private bool gyroEnabled;
+   private Gyroscope gyro;
+   private GameObject cameraContainer;
+   private Quaternion rot;
+
+    private void Start(){
+        Debug.Log("Start");
+        
+        cameraContainer = new GameObject("Camera container");
+        cameraContainer.transform.position = transform.position;
+        transform.SetParent(cameraContainer.transform);
+
+        gyroEnabled = EnableGyro();
+    }
+
+    private bool EnableGyro(){
+        if(SystemInfo.supportsGyroscope){
+            gyro = Input.gyro;
+            gyro.enabled = true;
+            cameraContainer.transform.rotation = Quaternion.Euler(90f,180f,0);
+            rot = new Quaternion(0,0,1,0);
+
+            return true;
+        }
+        return false;
+    }
+    
+    private void Update(){
+        if(gyroEnabled){
+            transform.localRotation = gyro.attitude * rot;
+        }
+    }
+}
--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -4,36 +4,15 @@ using UnityEngine;
 
 public class InputManager : MonoBehaviour
 {
-   private bool gyroEnabled;
-   private Gyroscope gyro;
-   private GameObject cameraContainer;
-   private Quaternion rot;
-
-    private void Start(){
-        Debug.Log("Start");
+    // Start is called before the first frame update
+    void Start()
+    {
         
-        cameraContainer = new GameObject("Camera container");
-        cameraContainer.transform.position = transform.position;
-        transform.SetParent(cameraContainer.transform);
-
-        gyroEnabled = EnableGyro();
     }
 
-    private bool EnableGyro(){
-        if(SystemInfo.supportsGyroscope){
-            gyro = Input.gyro;
-            gyro.enabled = true;
-            cameraContainer.transform.rotation = Quaternion.Euler(90f,180f,0);
-            rot = new Quaternion(0,0,1,0);
-
-            return true;
-        }
-        return false;
-    }
-    
-    private void Update(){
-        if(gyroEnabled){
-            transform.localRotation = gyro.attitude * rot;
-        }
+    // Update is called once per frame
+    void Update()
+    {
+        
     }
 }

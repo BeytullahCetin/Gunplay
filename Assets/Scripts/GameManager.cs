02f0a91ec9d631dd04c9600a3bbae268477c0a91
abdullahhhkeskinn <abdullahhhkeskinn@gmail.com>
2021-12-11 21:20:44 +0300

    Game environment fixes

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,9 +1,12 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
+
     void Start()
     {
+        SceneManager.LoadScene("Environment", LoadSceneMode.Additive);
         Cursor.visible = false;
         Cursor.lockState = CursorLockMode.Locked;
     }
